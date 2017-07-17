0454fa1d706035b323d4d8d743cf1f1fd9bf2d5b
Weiyi <wl1191@nyu.edu>
2017-07-17 14:25:08 -0400

    preliminary

--- preprocess.py
+++ preprocess.py
@@ -4,18 +4,18 @@ import random
 
 
 def build_data(src_dir, corpus_type, window):
-    corpra = {}
+    corpora = {}
     max_inst_in_doc = -1
     max_cluster_in_doc = -1
     max_sent_length = -1
     vocab = defaultdict(int)
     counters = {'sent_length': defaultdict(int),
-                'num_of_inst': defaultdict(int),
-                'num_of_cluster': defaultdict(int),
+                'num_of_inst': defaultdict(lambda: defaultdict(int)),
+                'num_of_cluster': defaultdict(lambda: defaultdict(int)),
                 'event_type': defaultdict(lambda: defaultdict(int)),
                 'event_subtype': defaultdict(lambda: defaultdict(int)),
                 'realis': defaultdict(lambda: defaultdict(int))}
-    fea_to_index_map = {'pos': {'######': 0},
+    map_fea_to_index = {'pos': {'######': 0},
                         'chunck': {'######': 0, 'O': 1},
                         'possibleTypes': {'NONE': 0},
                         'dep': {'NONE': 0},
@@ -33,7 +33,7 @@ def build_data(src_dir, corpus_type, window):
     inst_in_doc = 0
     cluster_in_doc = 0
     for type_ in corpus_type:
-        corpra[type_] = {}
+        corpora[type_] = {}
         print 'processing %s data' % type_
         with open(src_dir + '/' + type_ + '.txt', 'r') as fin:
             for line in fin:
@@ -41,18 +41,18 @@ def build_data(src_dir, corpus_type, window):
 
                 if line.startswith('#BeginOfDocument'):
                     current_doc = line[(line.find(' ') + 1):]
-                    corpra[type_][current_doc] = {'instances': [], 'coreference': []}
+                    corpora[type_][current_doc] = {'instances': [], 'coreference': []}
                     continue
 
                 if line == '#EndOfDocument':
-                    counters['num_of_inst'][inst_in_doc] += 1
+                    counters['num_of_inst'][type_][inst_in_doc] += 1
                     if max_inst_in_doc < inst_in_doc:
                         max_inst_in_doc = inst_in_doc
-                    counters['num_of_cluster'][cluster_in_doc] += 1
+                    counters['num_of_cluster'][type_][cluster_in_doc] += 1
                     if max_cluster_in_doc < cluster_in_doc:
                         max_cluster_in_doc = cluster_in_doc
 
-                    currentDoc = ''
+                    current_doc = ''
                     sent_id = -1
                     inst_in_doc = 0
                     cluster_in_doc = 0
@@ -71,8 +71,8 @@ def build_data(src_dir, corpus_type, window):
                         event_id = current_sent['eventId'][anchor_index]
                         if not event_id == 'NONE':
                             inst = parse_inst(current_sent, fea_placeholder, anchor_index, window, sent_id)
-                            corpra[type_][current_doc]['instances'] += [inst]
-                            corpra[type_][current_doc]['event_id'] += [event_id]
+                            corpora[type_][current_doc]['instances'] += [inst]
+                            corpora[type_][current_doc]['event_id'] += [event_id]
                             update_counters(type_, inst, counters)
                             inst_in_doc += 1
                     current_sent = defaultdict(list)
@@ -83,13 +83,52 @@ def build_data(src_dir, corpus_type, window):
                     if chain == 'Error':
                         print 'Incorrect coreference format in %s data:\nDocument: %s\n%s' % (type_, current_doc, line)
                         exit(0)
-                    corpra[type_][current_doc]['coreference'] += [chain]
+                    corpora[type_][current_doc]['coreference'] += [chain]
                     cluster_in_doc += 1
                     continue
 
-                if not parse_line(line, current_sent, fea_placeholder, fea_to_index_map, vocab):
+                if not parse_line(line, current_sent, fea_placeholder, map_fea_to_index, vocab):
                     print 'Incorrect line format in %s data:\nDocument: %s\n%s' % (type_, current_doc, line)
                     exit(0)
+
+    print_stats(corpus_type, counters, map_fea_to_index, max_inst_in_doc, max_cluster_in_doc, max_sent_length)
+
+
+def print_stats(corpus_type, counters, map_fea_to_index, max_inst_in_doc, max_cluster_in_doc, max_sent_length):
+    print '==============================Stats=============================='
+    print 'Distribution of number of instances in the corpora:'
+    for type_ in corpus_type:
+        print type_, ':\n', counters['num_of_inst'][type_]
+        print 'Total: ', sum(counters['num_of_inst'][type_].values()), '\n'
+    print '-----------------------------------------------------------------'
+    print 'Max number of instances in one doc: ', max_inst_in_doc
+    print '================================================================='
+    print 'Distribution of number of clusters in the corpora:'
+    for type_ in corpus_type:
+        print type_, ':\n', counters['num_of_cluster'][type_]
+        print 'Total: ', sum(counters['num_of_cluster'][type_].values()), '\n'
+    print '-----------------------------------------------------------------'
+    print 'Max number of clusters in one doc: ', max_cluster_in_doc
+    print '================================================================='
+    print 'Distribution of sentence lengths in the corpora:'
+    print counters['sent_length']
+    print '-----------------------------------------------------------------'
+    print 'Max length of a sentence: ', max_sent_length
+    print '================================================================='
+
+    def print_feature_stats(fea):
+        map_index_to_fea = {}
+        for item in map_fea_to_index[fea]:
+            map_index_to_fea[map_fea_to_index[fea][item]] = item
+        print 'Distribution of %s in the corpora:' % fea
+        for type_ in corpus_type:
+            print type_
+            for item_index in map_index_to_fea:
+                print '#', item_index, ' : ', counters[fea][type_]
+        print '================================================================='
+
+    for fea in ['event_type', 'event_subtype', 'realis']:
+        print_feature_stats(fea)
 
 
 def update_counters(type_, inst, counters):
@@ -138,7 +177,7 @@ def parse_coreference(line):
     return chain
 
 
-def parse_line(line, current_sent, fea_placeholder, fea_to_index_map, vocab):
+def parse_line(line, current_sent, fea_placeholder, map_fea_to_index, vocab):
     def update_map(msg, feature, feature_map):
         if feature not in feature_map:
             index = len(feature_map)
@@ -165,45 +204,45 @@ def parse_line(line, current_sent, fea_placeholder, fea_to_index_map, vocab):
         fea_placeholder['word'] = '######'
 
     pos = entries[5]
-    update_map('POS', pos, fea_to_index_map['pos'])
-    current_sent['pos'] += [fea_to_index_map['pos'][pos]]
+    update_map('POS', pos, map_fea_to_index['pos'])
+    current_sent['pos'] += [map_fea_to_index['pos'][pos]]
     if 'pos' not in fea_placeholder:
         fea_placeholder['pos'] = 0
 
     chunk = entries[6]
-    update_map('CHUNK', chunk, fea_to_index_map['chunk'])
-    current_sent['chunk'] += [fea_to_index_map['chunk'][chunk]]
+    update_map('CHUNK', chunk, map_fea_to_index['chunk'])
+    current_sent['chunk'] += [map_fea_to_index['chunk'][chunk]]
     if 'chunk' not in fea_placeholder:
         fea_placeholder['chunk'] = 0
 
     possible_types = entries[9].split()
     for type_ in possible_types:
-        update_map('POSSIBLE TYPE', type_, fea_to_index_map['possibleTypes'])
-    current_sent['possibleTypes'] += [[fea_to_index_map['possibleTypes'][type_] for type_ in possible_types]]
+        update_map('POSSIBLE TYPE', type_, map_fea_to_index['possibleTypes'])
+    current_sent['possibleTypes'] += [[map_fea_to_index['possibleTypes'][type_] for type_ in possible_types]]
     if 'possibleTypes' not in fea_placeholder: fea_placeholder['possibleTypes'] = []
 
     dep_paths = entries[12].split()
     for dep in dep_paths:
-        update_map('DEP', dep, fea_to_index_map['dep'])
-    current_sent['dep'] += [[fea_to_index_map['dep'][dep] for dep in dep_paths]]
+        update_map('DEP', dep, map_fea_to_index['dep'])
+    current_sent['dep'] += [[map_fea_to_index['dep'][dep] for dep in dep_paths]]
     if 'dep' not in fea_placeholder:
         fea_placeholder['dep'] = []
 
     nonref = entries[13]
-    update_map('NONREF', nonref, fea_to_index_map['nonref'])
-    current_sent['nonref'] += [fea_to_index_map['nonref'][nonref]]
+    update_map('NONREF', nonref, map_fea_to_index['nonref'])
+    current_sent['nonref'] += [map_fea_to_index['nonref'][nonref]]
     if 'nonref' not in fea_placeholder:
         fea_placeholder['nonref'] = 0
 
     title = entries[14]
-    update_map('TITLE', title, fea_to_index_map['title'])
-    current_sent['title'] += [fea_to_index_map['title'][title]]
+    update_map('TITLE', title, map_fea_to_index['title'])
+    current_sent['title'] += [map_fea_to_index['title'][title]]
     if 'title' not in fea_placeholder:
         fea_placeholder['title'] = 0
 
     eligible = entries[15]
-    update_map('ELIGIBLE', eligible, fea_to_index_map['eligible'])
-    current_sent['eligible'] += [fea_to_index_map['eligible'][eligible]]
+    update_map('ELIGIBLE', eligible, map_fea_to_index['eligible'])
+    current_sent['eligible'] += [map_fea_to_index['eligible'][eligible]]
     if 'eligible' not in fea_placeholder:
         fea_placeholder['eligible'] = 0
 
@@ -211,20 +250,20 @@ def parse_line(line, current_sent, fea_placeholder, fea_to_index_map, vocab):
     current_sent['binaryFeatures'] += [binary_features]
 
     event_type = entries[17]
-    update_map('EVENT TYPE', event_type, fea_to_index_map['type'])
-    current_sent['type'] += [fea_to_index_map['type'][event_type]]
+    update_map('EVENT TYPE', event_type, map_fea_to_index['type'])
+    current_sent['type'] += [map_fea_to_index['type'][event_type]]
 
     event_subtype = entries[18]
-    update_map('EVENT SUBTYPE', event_subtype, fea_to_index_map['subtype'])
-    current_sent['subtype'] += [fea_to_index_map['subtype'][event_subtype]]
+    update_map('EVENT SUBTYPE', event_subtype, map_fea_to_index['subtype'])
+    current_sent['subtype'] += [map_fea_to_index['subtype'][event_subtype]]
 
     event_realis = entries[19]
     if event_realis == 'NONE':
         current_sent['realis'] += [-1]
     else:
         event_realis = event_realis.lower()
-        update_map('EVENT REALIS', event_realis, fea_to_index_map['realis'])
-        current_sent['realis'] += [fea_to_index_map['realis'][event_realis]]
+        update_map('EVENT REALIS', event_realis, map_fea_to_index['realis'])
+        current_sent['realis'] += [map_fea_to_index['realis'][event_realis]]
 
     event_event_id = entries[20]
     current_sent['eventId'] += [event_event_id]
@@ -245,7 +284,7 @@ def main():
     corpus_type = ["train", "valid", "test"]
     window = 31
     print "loading data...\n"
-    # revs, fea_to_index_map, vocab, nodeFetCounter = build_data(src_dir, corpus_type, window)
+    # revs, map_fea_to_index, vocab, nodeFetCounter = build_data(src_dir, corpus_type, window)
 
 
 if __name__ == '__main__':
